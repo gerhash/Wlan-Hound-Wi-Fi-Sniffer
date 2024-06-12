f7d78b2e33ceb66b82db8d07ebdeb57a1cff8163
G#sh <62940515+gerhash@users.noreply.github.com>
2024-06-12 15:28:48 +0200

    snd

--- main.py
+++ main.py
@@ -2,66 +2,41 @@ import os
 import re
 import psutil
 from scapy.all import sniff
+from colorama import init, Fore
+from ascii import *
 
-# ASCII Art for the intro screen
-ascii_art = r"""
-
-  _____            ____               ____  _____   ______          ____   ____         _____     ____   ____  _____   ______        _____   
- |\    \   _____  |    |         ____|\   \|\    \ |\     \        |    | |    |   ____|\    \   |    | |    ||\    \ |\     \   ___|\    \  
- | |    | /    /| |    |        /    /\    \\\    \| \     \       |    | |    |  /     /\    \  |    | |    | \\    \| \     \ |    |\    \ 
- \/     / |    || |    |       |    |  |    |\|    \  \     |      |    |_|    | /     /  \    \ |    | |    |  \|    \  \     ||    | |    |
- /     /_  \   \/ |    |  ____ |    |__|    | |     \  |    |      |    .-.    ||     |    |    ||    | |    |   |     \  |    ||    | |    |
-|     // \  \   \ |    | |    ||    .--.    | |      \ |    |      |    | |    ||     |    |    ||    | |    |   |      \ |    ||    | |    |
-|    |/   \ |    ||    | |    ||    |  |    | |    |\ \|    |      |    | |    ||\     \  /    /||    | |    |   |    |\ \|    ||    | |    |
-|\ ___/\   \|   /||____|/____/||____|  |____| |____||\_____/|      |____| |____|| \_____\/____/ ||\___\_|____|   |____||\_____/||____|/____/|
-| |   | \______/ ||    |     |||    |  |    | |    |/ \|   ||      |    | |    | \ |    ||    | /| |    |    |   |    |/ \|   |||    /    | |
- \|___|/\ |    | ||____|_____|/|____|  |____| |____|   |___|/      |____| |____|  \|____||____|/  \|____|____|   |____|   |___|/|____|____|/ 
-    \(   \|____|/   \(    )/     \(      )/     \(       )/          \(     )/       \(    )/        \(   )/       \(       )/    \(    )/   
-     '      )/       '    '       '      '       '       '            '     '         '    '          '   '         '       '      '    '    
-            '                                                                                                                                
-    Wlan Hound by G#sh                                                                                           Wi-Fi Network Packet Sniffer
-
-                                                           |\|\
-                                                          ..    \       .
-                                                        o--     \\    / @)
-                                                         v__///\\\\__/ @
-                                                           {           }
-                                                            {  } \\\{  }
-                                                            <_|      <_|                                                                
-
-                                                         github.link.link                     
-                                                                                    
-"""
+# Initialize colorama
+init(autoreset=True)
 
 
 def packet_callback(packet):
-    print(packet.summary())  # Print a summary of the packet
-
+    print(Fore.GREEN + packet.summary())  # Print a summary of the packet in green
 
 def start_sniffer(interface):
     print(f"Starting sniffing on interface {interface}...")
     sniff(iface=interface, prn=packet_callback, store=0)
 
-
 if __name__ == "__main__":
     # Intro screen
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(ascii_art)
+    print(Fore.LIGHTGREEN_EX + ascii_art)  # Print ASCII art in yellow
+    print(Fore.LIGHTYELLOW_EX + ascii_art2)  # Print ASCII art in yellow
+    print(Fore.LIGHTWHITE_EX + ascii_art3)  # Print ASCII art in yellow
 
-    input("Press Enter to start the network sniffer...")
+    input(Fore.CYAN + "Press Enter to start the network sniffer...")  # Print prompt in cyan
 
     # Get and print the list of available network interfaces
     interfaces = psutil.net_if_addrs()
     wifi_interfaces = []
 
-    print("Available network interfaces:")
+    print(Fore.MAGENTA + "Available network interfaces:")  # Print header in magenta
     for iface in interfaces:
         print(iface)
         if re.search(r"Wi-Fi|Wireless|wlan", iface, re.IGNORECASE):
             wifi_interfaces.append(iface)
 
     # Show the available Wi-Fi interfaces
-    print("\nAvailable Wi-Fi interfaces:")
+    print(Fore.BLUE + "\nAvailable Wi-Fi interfaces:")  # Print header in blue
     for iface in wifi_interfaces:
         print(iface)
 
@@ -70,4 +45,4 @@ if __name__ == "__main__":
         interface = wifi_interfaces[0]  # Select the first available Wi-Fi interface
         start_sniffer(interface)
     else:
-        print("No Wi-Fi interfaces found.")
+        print(Fore.RED + "No Wi-Fi interfaces found.")  # Print error message in red
